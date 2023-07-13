b11283af3e240ab144324abf68f7082cce487632
dhzmqjf <dhzmqjf@naver.com>
2023-07-13 18:16:09 +0900

    배포 후 로그인 문제 수정

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,7 +1,11 @@
-import { all, fork } from "redux-saga/effects";
+import axios from 'axios';
+import { all, fork } from 'redux-saga/effects';
 
-import postSage from "./post";
-import userSaga from "./user";
+import postSage from './post';
+import userSaga from './user';
+
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
   yield all([fork(userSaga), fork(postSage)]);
