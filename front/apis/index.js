5a89d333037aa06ca5d1157c93426e930bdf15fd
dhzmqjf <dhzmqjf@naver.com>
2023-07-13 22:28:18 +0900

    api 설정 변경

--- front/apis/index.js
+++ front/apis/index.js
@@ -1,90 +1,81 @@
 import axios from 'axios';
 
-const getApiurl = () => process.env.NEXT_PUBLIC_API_URL;
-axios.defaults.withCredentials = true;
+const getApiurl = () => process.env.NEXT_PUBLIC_API_LOCAL_URL;
+// axios.defaults.withCredentials = true;
+
+export const api = axios.create({
+  baseURL: getApiUrl(),
+  withCredentials: true,
+});
 
 //로그인
-export const login = (data) =>
-  axios.post(`${getApiurl()}/user/login`, { ...data });
+export const login = (data) => api.post(`/user/login`, { ...data });
 
 // 회원가입
-export const signup = (data) => axios.post(`${getApiurl()}/user`, data);
+export const signup = (data) => api.post(`/user`, data);
 
 // 로그아웃
-export const logout = () => axios.post(`${getApiurl()}/user/logout`);
+export const logout = () => api.post(`/user/logout`);
 
 // 내정보
-export const myInfo = () => axios.get(`${getApiurl()}/user`);
+export const myInfo = () => api.get(`/user`);
 
 // 메인 게시글
-export const posts = (lastId) =>
-  axios.get(`${getApiurl()}/posts?lastId=${lastId || 0}`);
+export const posts = (lastId) => api.get(`/posts?lastId=${lastId || 0}`);
 
 // 게시글 작성
-export const addPost = (data) => axios.post(`${getApiurl()}/post`, data);
+export const addPost = (data) => api.post(`/post`, data);
 
 // 게시글 삭제
-export const removePost = (data) => axios.delete(`${getApiurl()}/post/${data}`);
+export const removePost = (data) => api.delete(`/post/${data}`);
 
 // 게시글 수정
-export const updatePost = (data) =>
-  axios.patch(`${getApiurl()}/post/${data.PostId}`, data);
+export const updatePost = (data) => api.patch(`/post/${data.PostId}`, data);
 
 // 이미지 업로드
-export const uploadImage = (data) =>
-  axios.post(`${getApiurl()}/post/images`, data);
+export const uploadImage = (data) => api.post(`/post/images`, data);
 
 // 좋아요
-export const like = (data) => axios.patch(`${getApiurl()}/post/${data}/like`);
+export const like = (data) => api.patch(`/post/${data}/like`);
 
 // 좋아요 취소
-export const unLike = (data) =>
-  axios.delete(`${getApiurl()}/post/${data}/like`);
+export const unLike = (data) => api.delete(`/post/${data}/like`);
 
 // 댓글
-export const comment = (data) =>
-  axios.post(`${getApiurl()}/post/${data.postId}/comment`, data);
+export const comment = (data) => api.post(`/post/${data.postId}/comment`, data);
 
 // 리트윗 하기
-export const retweet = (data) =>
-  axios.post(`${getApiurl()}/post/${data}/retweet`);
+export const retweet = (data) => api.post(`/post/${data}/retweet`);
 
 // 해시태그 가져오기
 export const hashtag = (data, lastId) =>
-  axios.get(
-    `${getApiurl()}/hashtag/${encodeURIComponent(data)}?lastId=${lastId || 0}`
-  );
+  api.get(`/hashtag/${encodeURIComponent(data)}?lastId=${lastId || 0}`);
 
 // 팔로우 하기
-export const addFollowing = (data) =>
-  axios.patch(`${getApiurl()}/user/${data}/follow`);
+export const addFollowing = (data) => api.patch(`/user/${data}/follow`);
 
 // 팔로우 취소
-export const removeFollowing = (data) =>
-  axios.delete(`${getApiurl()}/user/${data}/follow`);
+export const removeFollowing = (data) => api.delete(`/user/${data}/follow`);
 
 // 팔로워 취소
-export const removeFollower = (data) =>
-  axios.delete(`${getApiurl()}/user/follower/${data}`);
+export const removeFollower = (data) => api.delete(`/user/follower/${data}`);
 
 // 팔로우 리스트
-export const followingsList = (data) =>
-  axios.get(`${getApiurl()}/user/followings`, data);
+export const followingsList = (data) => api.get(`/user/followings`, data);
 
 // 팔로워 리스트
-export const followersList = (data) =>
-  axios.get(`${getApiurl()}/user/followers`, data);
+export const followersList = (data) => api.get(`/user/followers`, data);
 
 // 유저 정보
-export const userInfo = (data) => axios.get(`${getApiurl()}/user/${data}`);
+export const userInfo = (data) => api.get(`/user/${data}`);
 
 // 닉네임 변경
 export const nicknameEdit = (data) =>
-  axios.patch(`${getApiurl()}/user/nickname`, { nickname: data });
+  api.patch(`/user/nickname`, { nickname: data });
 
 // 선택 유저 게시글
 export const userPost = (data, lastId) =>
-  axios.get(`${getApiurl()}/user/${data}/posts?lastId=${lastId || 0}`);
+  api.get(`/user/${data}/posts?lastId=${lastId || 0}`);
 
 // 단일 게시글
-export const singlePost = (data) => axios.get(`${getApiurl()}/post/${data}`);
+export const singlePost = (data) => api.get(`/post/${data}`);
