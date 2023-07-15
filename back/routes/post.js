fc8678410343b1653ce4e444b5bf9889233a4067
dhzmqjf <dhzmqjf@naver.com>
2023-07-15 18:57:47 +0900

    image upload multer-s3 -> 3version 2version 다운그레이드

--- back/routes/post.js
+++ back/routes/post.js
@@ -24,7 +24,7 @@ AWS.config.update({
 const upload = multer({
   storage: multerS3({
     s3: new AWS.S3(),
-    bucket: "mytweeter-s3",
+    bucket: "mytweeter-shop-s3",
     key(req, file, cb) {
       cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
